63ea7e8240fb2fc42f6c88a59622a11d7badc495
DESKTOP-0H3FLM5\임동건 <rksk4687@naver.com>
2022-09-07 23:04:38 +0900

    todo-list

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,155 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <form class="register" @submit.prevent="handleTodoSubmit">
+        <p>TodoList</p>
+        <input type="text" id="register_text" placeholder="할일을 등록하세요." required>
+        <button type="submit" id ="resiger_button">등록</button>
+    </form>
+    <form>
+        <ul class="list"></ul>
+    </form>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+
+interface Todo {
+  text: string,
+  id: number,
+  check: boolean
+ 
+}
+@Options({
   props: {
     msg: String
   }
+})
+export default class HelloWorld extends Vue {
+
+saveTodos():void {
+  localStorage.setItem(this.TODOS_KEY, JSON.stringify(this.todos));
+}
+
+insertTodo(newTodo: Todo) {
+  const list:any = document.querySelector('.list');
+  console.log(this.todos);
+  const li = document.createElement('li');
+  const input = document.createElement('input');
+  const span = document.createElement('span');
+  input.id = newTodo.id as unknown as string;
+  input.addEventListener('click', this.checkEnd);
+
+  if (newTodo.check) {
+    input.checked = true;
+    li.classList.toggle('checked');
+  }
+
+  span.innerText = newTodo.text;
+  input.type = 'checkbox';
+  li.appendChild(input);
+  li.appendChild(span);
+  list.prepend(li);
+}
+
+ checkEnd(event: MouseEvent) {
+  let myInput = event.target as HTMLInputElement;
+
+  for (let i = 0; i < this.todos.length; i++) {
+    if (this.todos[i].id.toString() == myInput.id) {
+      this.todos[i].check = myInput.checked;
+      (myInput.parentNode as HTMLElement).classList.toggle('checked');
+      break;
+    }
+  }
+  this.saveTodos();
+}
+
+handleTodoSubmit() {
+  const register_text:HTMLInputElement = document.querySelector('#register_text') as HTMLInputElement; 
+  console.log(register_text);
+  const newTodo: string = register_text?.value as string;
+  const newTodoObj:Todo = {
+    text:newTodo,
+    id:Date.now(),
+    check:false
+  };
+  (register_text as HTMLInputElement).value = '';
+  this.todos.push(newTodoObj);
+  this.insertTodo(newTodoObj);
+  this.saveTodos();
+}
+
+TODOS_KEY = 'todos';
+todos:Array<Todo> = [];
+mounted(){
+  let savedTodos = localStorage.getItem(this.TODOS_KEY);
+  console.log(savedTodos)
+if (savedTodos) {
+  const parsedTodos = JSON.parse(savedTodos);
+  this.todos = parsedTodos;
+  this.todos.forEach(this.insertTodo);
+}
+
+}
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+body {
+    text-align: center;
+  }
+p{
+    font-size: 40px;
+    font-weight: bold;
+    margin-top: 0px;
+    margin-bottom: 20px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    background-color: deeppink;
+    color: white;
+    border-radius: 20px 20px 0px 0px / 20px 20px 0px 0px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+ div{
+    display: inline-block;
+    border: 2px solid deeppink;
+    border-radius: 20px 20px 20px 20px / 20px 20px 20px 20px;
+    width: 550px;
+    height: 700px;
+    background-color: snow;
+ }
+  #register_text {
+    width: 350px;
+    height: 20px;
+    border-color: deeppink;
+  }
+  button {
+    width: 50px;
+    height: 30px;
+    border : none;
+    background-color: deeppink;
+    color: white;
+    font-weight: bold;
+    border-radius: 5px 5px 5px 5px / 5px 5px 5px 5px;
+  }
+  ul {
+    list-style: none;
+    text-align: left;
+    margin-left: 10%;
+    font-weight: bold;
+    color:deeppink;
+  }
+  li {
+    margin-bottom: 10px;
+  }
+  
+  li.checked {
+    font-weight: lighter;
+    text-decoration: line-through;
+    font-style: italic;
+    color:gray
+  }
 </style>
